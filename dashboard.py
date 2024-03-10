2ffe981654929f5aa3aa67d62c87ab82374663ea
gloryindahs <162136079+gloryindahs@users.noreply.github.com>
2024-03-10 21:39:26 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,21 +8,19 @@ sns.set(style='dark')
 day_df = pd.read_csv("day.csv")
 
 st.header('Bike Share Dashboard :sparkles:')
+
+col1, col2 = st.columns(2)
 st.subheader('Rata-Rata Jumlah Sewa Sepeda Berdasarkan Musim dan Cuaca')
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 6))
 
-# Grafik pertama: Musim
-sns.barplot(x='season', y='cnt', data=result_df, palette="Blues_d", ax=axes[0])
-axes[0].set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Musim')
-axes[0].set_xlabel('Musim')
-axes[0].set_ylabel('Rata-rata Jumlah Sepeda')
+with col1:
+season_mapping = {1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'}
+result_df = day_df.groupby(by="season").mean()['cnt'].sort_values(ascending=False).reset_index(drop=False)
+result_df['season'] = result_df['season'].map(season_mapping)
+print(result_df)  
 
-# Grafik kedua: Kondisi Cuaca
-sns.barplot(x='weathersit', y='cnt', data=day_df, palette="Blues_d", ax=axes[1])
-axes[1].set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Kondisi Cuaca')
-axes[1].set_xlabel('Kondisi Cuaca')
-axes[1].set_ylabel('Rata-rata Jumlah Sepeda')
-
-# Tampilkan plot
-plt.tight_layout()
+plt.figure(figsize=(10, 6))
+sns.barplot(x='season', y='cnt', data=result_df, palette="Blues_d")
+plt.title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Musim')
+plt.xlabel('Musim')
+plt.ylabel('Rata-rata Jumlah Sepeda')
 plt.show()
