6e844f902afcae6f823809111f286ec001e497cb
gloryindahs <162136079+gloryindahs@users.noreply.github.com>
2024-03-10 21:40:01 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,6 +13,7 @@ col1, col2 = st.columns(2)
 st.subheader('Rata-Rata Jumlah Sewa Sepeda Berdasarkan Musim dan Cuaca')
 
 with col1:
+  
 season_mapping = {1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'}
 result_df = day_df.groupby(by="season").mean()['cnt'].sort_values(ascending=False).reset_index(drop=False)
 result_df['season'] = result_df['season'].map(season_mapping)
