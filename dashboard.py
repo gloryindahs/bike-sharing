127112926dfa690de51655d68d3c3fd78cafd540
gloryindahs <162136079+gloryindahs@users.noreply.github.com>
2024-03-11 00:30:40 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -18,7 +18,7 @@ end_date = current_date
 
 with st.sidebar:
     # Menambahkan logo perusahaan
-     st.image("https://github.com/gloryindahs/bike-sharing/blob/main/bike.jpg", width=100)
+     st.image("https://github.com/gloryindahs/bike-sharing/blob/main/bike.jpg")
     
     # Mengambil start_date & end_date dari date_input
     selected_dates = st.date_input(
