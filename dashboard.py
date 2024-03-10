8a733bd35ac8d3b6e9bc07328198999fc6c01052
gloryindahs <162136079+gloryindahs@users.noreply.github.com>
2024-03-10 21:48:49 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,31 +1,27 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
-import plotly.express as px
+import plotly.express as px  # Import plotly express
+from babel.numbers import format_currency
+sns.set(style='dark')
 
-# Load your data
 day_df = pd.read_csv("day.csv")
 
-# Set up Streamlit
 st.header('Bike Share Dashboard :sparkles:')
 
-# Create two columns layout
 col1, col2 = st.columns(2)
-
-# Subheader for the section
 st.subheader('Rata-Rata Jumlah Sewa Sepeda Berdasarkan Musim dan Cuaca')
 
-# Left column: Season-wise bike share count
-with col1:
+with col1:  
     season_count = day_df.groupby("season_label")["cnt"].sum().reset_index()
-    fig_season_count = px.bar(season_count, x="season_label", y="cnt", title="Jumlah Penyewa berdasarkan Musim")
-    
-    # Display the plot in Streamlit
-    st.plotly_chart(fig_season_count, use_container_width=True, height=400, width=600)
+    fig_season_count = px.bar(season_count, x="season_label",
+                              y="cnt", title="Jumlah Penyewa berdasarkan Musim")
+    st.plotly_chart(fig_season_count, use_container_width=True,
+                    height=400, width=600)
 
-# Right column: Weather situation-wise bike share count
 with col2:
     weather_count = day_df.groupby("weathersit")["cnt"].sum().reset_index()
-    fig_weather_count = px.bar(weather_count, x="weathersit", y="cnt", title="Jumlah Penyewa berdasarkan Cuaca")
-    
-    # Display the plot in Streamlit
+    fig_weather_count = px.bar(weather_count, x="weathersit",
+                               y="cnt", title="Jumlah Penyewa berdasar Cuaca")
     st.plotly_chart(fig_weather_count, use_container_width=True, height=400, width=800)
