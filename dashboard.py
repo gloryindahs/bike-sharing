39a8063360d17a1dfffa06c5dbf3938a90290080
gloryindahs <162136079+gloryindahs@users.noreply.github.com>
2024-03-10 21:56:44 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,26 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import plotly.express as px  # Import plotly express
 from babel.numbers import format_currency
+
 sns.set(style='dark')
 
 day_df = pd.read_csv("day.csv")
 
 st.header('Bike Share Dashboard :sparkles:')
-
-col1, col2 = st.columns(2)
 st.subheader('Rata-Rata Jumlah Sewa Sepeda Berdasarkan Musim dan Cuaca')
 
-with col1:  
-    season_count = day_df.groupby("season_label")["cnt"].sum().reset_index()
-    fig_season_count = px.bar(season_count, x="season_label",
-                              y="cnt", title="Jumlah Penyewa berdasarkan Musim")
-    st.plotly_chart(fig_season_count, use_container_width=True,
-                    height=400, width=600)
+# Membagi layar menjadi dua kolom
+col1, col2 = st.columns(2)
 
+# Scatter plot untuk Suhu
+with col1:
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='temp', y='cnt', data=day_df)
+    plt.title('Korelasi Suhu vs Jumlah Sewa Sepeda')
+    plt.xlabel('Suhu')
+    plt.ylabel('Jumlah Sepeda')
+    st.pyplot()
+
+# Scatter plot untuk Kelembaban
 with col2:
-    weather_count = day_df.groupby("weathersit")["cnt"].sum().reset_index()
-    fig_weather_count = px.bar(weather_count, x="weathersit",
-                               y="cnt", title="Jumlah Penyewa berdasar Cuaca")
-    st.plotly_chart(fig_weather_count, use_container_width=True, height=400, width=800)
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='hum', y='cnt', data=day_df)
+    plt.title('Korelasi Kelembaban vs Jumlah Sewa Sepeda')
+    plt.xlabel('Kelembaban')
+    plt.ylabel('Jumlah Sepeda')
+    st.pyplot()
